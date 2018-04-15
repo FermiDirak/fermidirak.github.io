997dc700a2e4b488f1b5d6ed01ddfdec64d9ef01
fermi_dirac <ManueleBryan@gmail.com>
2018-04-15 01:11:16 -0700

    portfolio templating engine set up

--- templates/portfolio.js
+++ templates/portfolio.js
@@ -1,72 +1,49 @@
+/* example project object:
+ *  let sampleProject = {
+ *    imageUrl: './img/project.png',
+ *    title: 'app_name',
+ *    desc: 'app_desc',
+ *    tags: ['tag1', 'tag2', 'tag3'],
+ *    link: 'https://www.github...',
+ *  }
+ */
+
+let projects = [
+  {
+    imageUrl: './img/e3s.jpg ',
+    title: 'FoodSpots',
+    desc: 'An app for users to share their favorite food spots in the city',
+    link: '',
+    tags: ['react', 'redux', 'express', 'serverless', 'mysql'],
+  },
+]
+
+/** creates a portfolio piece template
+ * @param project {object} The data of the project to fill portfolio piece with
+ * @return {string} the portfolio piece template */
+let portfolioPiece = (project) => {
+  return `
+    <article class="app-container">
+      <img class="app-image" src=${project.imageUrl}/>
+
+      <div class="app-container-content">
+        <h2 class="app-title">${project.title}</h2>
+        <p>${project.desc}</p>
+        <ul class="tag-list">
+          ${project.tags.map(label => `<li class="tag">${label}</li>`).join('')}
+        </ul>
+      </div>
+    </article>
+  `;
+}
+
 let portfolio = `
   <section class="portfolio">
 
 
   <h1>Portfolio</h1>
 
-
-  <article class="app-container">
-    <img class="app-image"/>
-
-    <div class="app-container-content">
-      <h2 class="app-title">App Title</h2>
-      <p>App description</p>
-      <ul class="tag-list">
-        <li class="tag">react</li>
-        <li class="tag">express</li>
-        <li class="tag">mysql</li>
-        <li class="tag">mongoose</li>
-      </ul>
-    </div>
-  </article>
-
-
-  <article class="app-container">
-    <img class="app-image"/>
-
-    <div class="app-container-content">
-      <h2 class="app-title">App Title</h2>
-      <p>App description</p>
-      <ul class="tag-list">
-        <li class="tag">react</li>
-        <li class="tag">express</li>
-        <li class="tag">mysql</li>
-        <li class="tag">mongoose</li>
-      </ul>
-    </div>
-  </article>
-
-
-    <article class="app-container">
-      <img class="app-image"/>
-
-      <div class="app-container-content">
-        <h2 class="app-title">App Title</h2>
-        <p>App description</p>
-        <ul class="tag-list">
-          <li class="tag">react</li>
-          <li class="tag">express</li>
-          <li class="tag">mysql</li>
-          <li class="tag">mongoose</li>
-        </ul>
-      </div>
-    </article>
-
-
-      <article class="app-container">
-        <img class="app-image"/>
-
-        <div class="app-container-content">
-          <h2 class="app-title">App Title</h2>
-          <p>App description</p>
-          <ul class="tag-list">
-            <li class="tag">react</li>
-            <li class="tag">express</li>
-            <li class="tag">mysql</li>
-            <li class="tag">mongoose</li>
-          </ul>
-        </div>
-      </article>
+  ${projects.map(project => portfolioPiece(project)).join('')}
 
   </section>
 `;

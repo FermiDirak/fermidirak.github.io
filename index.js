83ca1ca400b29ef350731e53d1e75a12b89f8b74
fermi_dirac <ManueleBryan@gmail.com>
2018-04-15 00:17:28 -0700

    routing works now

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-let contentDiv = document.getElementById('content');
-
-// window.location.hash = 'swag';
-
-let routes = {
-  '/index.html': homepage,
-  '/': homepage,
-  '/portfolio': portfolio,
-};
-
-let onNavItemClick = (itemName) => {
-  console.log(itemName);
-
-  window.location.pathname = itemName;
-  contentDiv.innerHTML = routes[itemName];
+let getPathname = function() {
+  let urlComponents = window.location.href.split('/');
+  return urlComponents[urlComponents.length - 1];
 }
 
-contentDiv.innerHTML = routes[window.location.pathname];
+let setPathname = function(pathname) {
+  let urlComponents = window.location.href.split('/');
+  urlComponents[urlComponents.length - 1] = pathname;
+  let url = urlComponents.join('/');
+
+  // window.location.href = url;
+
+  window.history.pushState({}, pathname, url);
+}
+
+window.onpopstate = () => {
+  let pathName = getPathname();
+  contentDiv.innerHTML = routes[pathName];
+}
+
+let contentDiv = document.getElementById('content');
+
+let routes = {
+  '': homepage,
+  'index.html': homepage,
+  'portfolio': portfolio,
+};
+
+
+let onNavItemClick = (pathName) => {
+  setPathname(pathName);
+  contentDiv.innerHTML = routes[pathName];
+}
+
+contentDiv.innerHTML = routes[getPathname()];

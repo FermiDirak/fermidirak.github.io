241ea1a77cd4644caed653995b699d450dd47c90
fermi_dirac <ManueleBryan@gmail.com>
2018-04-15 00:35:20 -0700

    changed flag to project pages

--- redirect.js
+++ redirect.js
@@ -31,4 +31,4 @@
   /* if current document is 404 page page, redirect to index.html otherwise resolve */
   document.title === '404' ? redirect() : resolve()
 
-}(window.location, window.projectPages || true ))
+}(window.location, window.projectPages || false ))
